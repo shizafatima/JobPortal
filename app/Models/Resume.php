60af7958ea64d60c0427eb9932c0d6238c598e3d
shizafatima <shizafatima31@gmail.com>
2025-12-08 10:02:12 +0500

    add resume model

---
+++ app/Models/Resume.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Resume extends Model
+{
+    //
+
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id', 'full_name', 'email', 'phone', 'linkedin', 'address',
+        'summary', 'experience', 'education', 'skills', 'certifications'
+    ];
+
+    protected $casts = [
+        'experience' => 'array',
+        'education' => 'array',
+        'skills' => 'array',
+        'certifications' => 'array',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
