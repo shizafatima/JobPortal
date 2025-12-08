9f99fd0bef342fe576d3ee4c81db76b14ffdd9dd
shizafatima <shizafatima31@gmail.com>
2025-12-08 10:03:50 +0500

    add resumes table to DB

---
+++ database/migrations/2025_12_05_063645_create_resumes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('resumes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('full_name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('linkedin')->nullable();
+            $table->string('address')->nullable();
+            $table->text('summary')->nullable();
+            $table->json('experience')->nullable();
+            $table->json('education')->nullable();
+            $table->json('skills')->nullable();
+            $table->json('certifications')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('resumes');
+    }
+};
