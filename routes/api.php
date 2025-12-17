9ab9da6025499d46b9848a6ec0332c80dd73787b
shizafatima <shizafatima31@gmail.com>
2025-12-17 14:10:28 +0500

    add routes of save, delete, get

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,14 @@
 use App\Http\Controllers\ResumeController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/resume/store', [ResumeController::class, 'store']);
+// Route::post('/resume/store', [ResumeController::class, 'store']);
 
-Route::middleware('auth')->group(function () {
-    Route::post('/resume/save', [ResumeController::class, 'saveResume']);
-    Route::delete('/resume/{resume}', [ResumeController::class, 'delete']);
-    Route::get('/resume/get', [ResumeController::class, 'getResume']);
-});
+// Route::middleware('auth')->group(function () {
+//     Route::post('/resume/save', [ResumeController::class, 'saveResume']);
+//     Route::delete('/resume/{resume}', [ResumeController::class, 'delete']);
+//     Route::get('/resume/get', [ResumeController::class, 'getResume']);
+// });
+
+Route::post('/resume/save', [ResumeController::class, 'saveResume']);
+Route::delete('/resume/{resume}', [ResumeController::class, 'delete']);
+Route::get('/resume/get', [ResumeController::class, 'getResume']);
