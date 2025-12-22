5867e4b30bf86ecbf154382eac55c690a01620c1
shizafatima <shizafatima31@gmail.com>
2025-12-22 13:48:21 +0500

    add meta tag for csrf token

--- resources/views/layouts/plain.blade.php
+++ resources/views/layouts/plain.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Resume Builder</title>
+    <title>Resume Form</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
