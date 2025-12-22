8bdc089d9a2290905a7da350a170a25a882ac9b9
shizafatima <shizafatima31@gmail.com>
2025-12-22 13:54:56 +0500

    add middleware 'auth:sanctum' for api routes

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,13 @@
 use App\Http\Controllers\ResumeController;
 use Illuminate\Support\Facades\Route;
 
-// Route::post('/resume/store', [ResumeController::class, 'store']);
 
-// Route::middleware('auth')->group(function () {
-//     Route::post('/resume/save', [ResumeController::class, 'saveResume']);
-//     Route::delete('/resume/{resume}', [ResumeController::class, 'delete']);
-//     Route::get('/resume/get', [ResumeController::class, 'getResume']);
-// });
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/resume/save', [ResumeController::class, 'saveResume']);
+    Route::delete('/resume/{resume}', [ResumeController::class, 'delete']);
+    Route::get('/resume/get', [ResumeController::class, 'getResume']);
+});
 
-Route::post('/resume/save', [ResumeController::class, 'saveResume']);
-Route::delete('/resume/{resume}', [ResumeController::class, 'delete']);
-Route::get('/resume/get', [ResumeController::class, 'getResume']);
+// Route::post('/resume/save', [ResumeController::class, 'saveResume']);
+// Route::delete('/resume/{resume}', [ResumeController::class, 'delete']);
+// Route::get('/resume/get', [ResumeController::class, 'getResume']);
